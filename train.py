a67e87872a677176559977b4b1326129e593c72a
Daniel Doyle <daniel.doyle@outlook.ie>
2023-12-09 22:17:23 +0000

    make cnn in train script

--- train.py
+++ train.py
@@ -1,26 +1,40 @@
 from dataset import TeamAssignmentDataset
 import torch
 import torch.nn as nn
-from torchvision import transforms
+import torch.nn.functional as F
 from torch.utils.data import DataLoader
-from torchvision.models import resnet50
 
-# TODO: initialise dataset
-# dataset = TeamAssignmentDataset(...)
+class EmbeddingCNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64 * 15 * 15, 512)  # Adjust dimensions according to your pooling layers
+        self.fc2 = nn.Linear(512, 1024)
 
-# TODO: DataLoader setup
-# dataloader = DataLoader(dataset, batch_size=..., shuffle=True)
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv3(x)), (2, 2))
+        x = x.view(-1, 64 * 15 * 15)  # Adjust dimensions according to your pooling layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
-# Model setup (using ResNet50 for feature extraction as an example)
-model = resnet50(pretrained=True)
-model.fc = nn.Linear(model.fc.in_features, 128)  # Adjust output features
+dataset = TeamAssignmentDataset()
+dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
+
+model = EmbeddingCNN()
 
 # Loss and optimizer
 criterion = nn.TripletMarginLoss(margin=1.0, p=2)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
+NUM_EPOCHS = 20
+
 # Training loop
-for epoch in range(num_epochs):
+for epoch in range(NUM_EPOCHS):
     for data in dataloader:
         anchor, positive, negative = data  # Your dataloader should return triplets
         anchor_out = model(anchor)
@@ -33,4 +47,4 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
     
-    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+    print(f"Epoch [{epoch+1}/{NUM_EPOCHS}], Loss: {loss.item()}")
